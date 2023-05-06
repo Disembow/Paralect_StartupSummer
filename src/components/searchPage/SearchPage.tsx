7e1d487b50ec82c0fc49ea0a3e60ba2ae34c84c2
Disembow <e_naletov@mail.ru>
2023-05-06 11:32:33 +0200

    fix: chanch .search component

--- src/components/searchPage/SearchPage.tsx
+++ src/components/searchPage/SearchPage.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Filters from './UI/Filters';
 import SearchBar from './UI/SearchBar';
-import style from './SearchPage.module.scss';
+import styles from './SearchPage.module.scss';
 
 const SearchPage = () => {
   return (
     <>
-      <div className={style.container}>
+      <div className={styles.container}>
         <Filters />
-        <SearchBar />
+        <div className={styles.search}>
+          <SearchBar />
+        </div>
       </div>
     </>
   );
--- src/components/searchPage/UI/SearchBar.tsx
+++ src/components/searchPage/UI/SearchBar.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import { Button, Input } from '@mantine/core';
 import { IconSearch } from '@tabler/icons-react';
-import styles from './SearchBar.module.scss';
-import '../../../styles/_globals.scss';
 
 const SearchBar = () => {
   return (
-    <div className={styles.search}>
-      <Input
-        width={'100%'}
-        icon={<IconSearch size="0.8rem" />}
-        placeholder="Введите название вакансии"
-        rightSection={
-          <Button type="button" right={'1.7rem'} h={'2rem'} w={'5.2rem'}>
-            Поиск
-          </Button>
-        }
-        radius="md"
-        size="lg"
-      />
-    </div>
+    <Input
+      width={'100%'}
+      icon={<IconSearch size="0.8rem" />}
+      placeholder="Введите название вакансии"
+      rightSection={
+        <Button type="button" right={'1.7rem'} h={'2rem'} w={'5.2rem'}>
+          Поиск
+        </Button>
+      }
+      radius="md"
+      size="lg"
+    />
   );
 };
 
