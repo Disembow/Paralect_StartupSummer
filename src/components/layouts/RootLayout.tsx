00c52369a86774d2ea2234b603ab09e13b8e13fc
Disembow <e_naletov@mail.ru>
2023-05-15 14:44:45 +0200

    feat: add scroll to top on every page

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Logo from './UI/Logo';
 import NavigationBar from './UI/NavigationBar';
 import './RootLayout.scss';
 import Overlay from './UI/Overlay';
 
 export default function RootLayout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Overlay />
