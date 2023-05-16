01575e47ea89def6907ae74430b6599e73167790
Disembow <e_naletov@mail.ru>
2023-05-16 15:11:44 +0200

    feat: add navigation by clicking on logo

--- src/components/layouts/UI/Logo.tsx
+++ src/components/layouts/UI/Logo.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './UI.module.scss';
 import classNames from 'classnames';
+import { useNavigate } from 'react-router';
 
 const Logo = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className={styles.logo}>
+    <div className={styles.logo} onClick={() => navigate('/')}>
       <div className={styles.logo__box}>
         <span className={classNames(styles.logo__item, styles.logo__item_left)}></span>
         <span className={classNames(styles.logo__item, styles.logo__item_right)}></span>
