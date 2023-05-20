33525d7cd10fc308617d56e9ddffec3496364f49
Disembow <e_naletov@mail.ru>
2023-05-20 13:03:23 +0200

    fix: add immediatly removing of favourits item after clicking on star

--- src/components/favoritesPage/FavoritesPage.tsx
+++ src/components/favoritesPage/FavoritesPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cards from '../searchPage/UI/Cards';
 import EmptyLayout from './UI/EmptyLayout';
 import { getFavourits } from '../../app/localStorage';
@@ -6,25 +6,27 @@ import StyledPagination from '../searchPage/UI/StyledPagination';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { setFavouritsCurrPage } from '../../app/slices/cardsSlice';
 import styles from './FavoritesPage.module.scss';
+import { TJobsDate } from '../../types/dataType';
 
 const FavoritesPage = () => {
   const dispatch = useAppDispatch();
+  const [favourits, setFavourits] = useState<TJobsDate[]>([]);
   const page = useAppSelector((state) => state.cards.favouritsCurrPage);
 
   const ITEMS_ON_FAVORITE_PAGE = 5;
-
-  const favourits = getFavourits();
-
   const total = Math.ceil(getFavourits().length / ITEMS_ON_FAVORITE_PAGE);
-
   const start = (page - 1) * ITEMS_ON_FAVORITE_PAGE;
   const end = start + ITEMS_ON_FAVORITE_PAGE;
+
+  useEffect(() => {
+    setFavourits(getFavourits().slice(start, end));
+  }, [end, favourits, start]);
 
   return (
     <div className={styles.wrapper}>
       {favourits.length > 0 ? (
         <>
-          <Cards data={favourits.slice(start, end)} />
+          <Cards data={favourits} />
           <StyledPagination
             total={total}
             onChange={(e) => dispatch(setFavouritsCurrPage(e))}
